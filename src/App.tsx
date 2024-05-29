8e6f878dbc70ab5bb679c9500fe490d86f5514fe
Vladislava <stdemidovez@gmail.com>
2024-05-30 00:21:49 +0300

    09-less

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header/>
+        <Technologies/>
     </div>
   );
 }
